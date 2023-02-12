435cfc0d2174a65a96a2867ff7a4a672757d377b
Levi Zoesch <lzoesch@gmail.com>
2023-02-11 23:55:28 -0800

    Create SetEnvironmentVariable.php
    Init Commit

---
+++ src/SetEnvironmentVariable.php
@@ -0,0 +1,20 @@
+
+namespace App\Console\Commands\Admin;
+
+use Illuminate\Console\Command;
+
+class SetEnvironmentVariable extends Command
+{
+    protected $signature = 'env:set {key} {value}';
+    protected $description = 'Set an environment variable';
+
+    public function handle(): void
+    {
+        $key = $this->argument('key');
+        $value = $this->argument('value');
+
+        putenv("$key=$value");
+
+        $this->info("Environment variable [$key] set to [$value]");
+    }
+}
